f630c644c65d25b089ca7a78c35fbdcdcf3e3151
Ashish Pandey <ashish.logicon@gmail.com>
2020-10-31 13:02:34 +0530

    gjv

--- TODEYME/ToDoListTableViewController.swift
+++ TODEYME/ToDoListTableViewController.swift
@@ -19,7 +19,7 @@ class ToDoListTableViewController: UITableViewController {
         super.viewDidLoad()
      
        loadItems()
-     
+     print("sdfdsfsdfsdf")
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
